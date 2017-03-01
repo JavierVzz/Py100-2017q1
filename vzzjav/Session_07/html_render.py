528f52d0ce9a076abfcc0eef345241154c2ea3d3
Javier Vazquez <vzz_jav@yahoo.com>
2017-02-28 17:58:30 -0800

    IN PROGRESS!!!

---
+++ vzzjav/Session_07/html_render.py
@@ -0,0 +1,15 @@
+import os, io
+
+
+class Element():
+
+    def __init__(self):
+        tag = "<html>"
+        ind = ""
+
+
+
+if __name__ == '__main__':
+    print("Direct access to " + os.path.basename(__file__))
+else:
+    print(os.path.basename(__file__) + " class instance")
---
+++ vzzjav/Session_07/run_html_render.py
@@ -0,0 +1,15 @@
+# Javier Vazquez
+# Grid Printer
+# Date
+# Description:
+
+import os, html_render
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    print(os.path.basename(__file__))
+    main()
